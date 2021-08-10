865d4831043190d6e932def9dc93cdddb877f7b4
SSTinYukk <13999730523@163.com>
2021-08-10 09:34:49 +0800

    C++

---
+++ Class/02 封装案例二-点和圆的关系.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Point
+{
+    public:
+    void setX(int x)
+    {
+        m_X=x;
+    }
+    
+    void setY(int y)
+    {
+        m_Y=y;
+    }
+    
+    int m_X;
+    int m_Y;
+}
+
+
+class Circle
+{
+    private:
+    
+};
+
+int main()
+{
+
+}
