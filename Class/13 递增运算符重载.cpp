22c6205e5d775184d9760cfcafa33a301cfdbfa4
SSTinYukk <13999730523@163.com>
2021-08-11 14:02:07 +0800

    C++

--- Class/13 递增运算符重载.cpp
+++ Class/13 递增运算符重载.cpp
@@ -13,6 +13,30 @@ public:
         m_Int++;
         return  temp;
     }
+
+    MyInteger& operator++()
+    {
+        m_Int++;
+        return *this;
+    }
     int m_Int;
 };
 
+ostream& operator<<(ostream &cout,MyInteger i)
+{
+    cout<<i.m_Int;
+    return cout;
+}
+
+ostream &operator<<(MyInteger &i, ostream &cout)
+{
+    cout << i.m_Int;
+    return cout;
+}
+
+int main()
+{
+    MyInteger i(1);
+    cout << ((i++)++)<<endl;
+    cout<<i<<endl;
+}
