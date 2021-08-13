ab28cd0f371f42e33aba73874ca73cc64a1ba673
SSTinYukk <13999730523@163.com>
2021-08-13 23:29:31 +0800

    C++

--- STL/02 string容器-字符串查找和替换.cpp
+++ STL/02 string容器-字符串查找和替换.cpp
@@ -1,1 +1,18 @@
-#
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string str1 = "abcdefg";
+
+    int pos =str1.find("de");
+
+    if(pos==-1)
+    {
+        cout<<"没找到"<<endl;
+    }else
+    {
+        cout<<"找到字符串 坐标:"<<pos<<endl;
+    }
+    return 0;
+}
