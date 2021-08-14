ccee77e79f85228b878349267f5741d5b0a262ac
SSTinYukk <13999730523@163.com>
2021-08-14 15:47:11 +0800

    C++

---
+++ STL/07 vector容器-预留空间.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+int main()
+{
+    vector<int>v;
+    int num=0;
+    int * p=NULL;
+    for(int i=0;i<100000;i++)
+    {
+        v.push_back(i);
+        if(p!=&v[0])
+        {
+            p=&v[0];
+            num++;
+        }
+    }
+    cout<<num<<endl;
+}
