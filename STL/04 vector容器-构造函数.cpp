bd54e31332f129f75c4f883cdc2175f466335734
SSTinYukk <13999730523@163.com>
2021-08-14 15:14:24 +0800

    C++

--- STL/04 vector容器-构造函数.cpp
+++ STL/04 vector容器-构造函数.cpp
@@ -13,10 +13,22 @@ void printVector(vector<int>&v)
 void test01()
 {
     vector<int>v1;
+    v1.insert(v1.begin(),2233);
+    v1.erase(v1.begin());
+
+    vector<int>v2(v1.begin(),v1.end());
+
+    vector<int>v3(10,100);
+
 
     for(int i=0;i<10;i++){
         v1.push_back(i);
     }
+    string a;
+    printVector(v1);
+    cout<<endl;
+    printVector(v2);
+    printVector(v3);
 }
 
 int main()
---
+++ STL/05 vector容器-数据存取.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+void test01()
+{
+    vector<int>v1;
+
+    for(int i=0;i<10;i++)
+    {
+        v1.push_back(i);
+    }
+
+    for(int i=0;i<v1.size();i++){
+        cout<<v1.at(i)<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    test01();
+    return 0;
+}
