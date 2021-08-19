536d6a4ceb0df360923e228bf3a68149f6c6f240
SSTinYukk <13999730523@163.com>
2021-08-19 19:22:46 +0800

    C++

--- STL/01 vector容器遍历.cpp
+++ STL/01 vector容器遍历.cpp
@@ -13,6 +13,9 @@ void test01()
     vector<int> v;
     v.push_back((10));
     v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    
 
     vector<int>::iterator pBegin =v.begin();
     vector<int>::iterator pEnd = v.end();
