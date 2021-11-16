b6de3108e883e2b7d3bb6e79d7b4097ea734fef5
SSTinYukk <13999730523@163.com>
2021-11-16 21:57:50 +0800

    feat go

--- GO/hello.go
+++ GO/hello.go
@@ -1,12 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Welcome to the playground!")
+	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
 
-	fmt.Println("The time is", time.Now())
+	// 截取切片使其长度为 0
+	s = s[:0]
+	printSlice(s)
+
+	// 拓展其长度
+	s = s[:4]
+	printSlice(s)
+
+	// 舍弃前两个值
+	s = s[2:]
+	printSlice(s)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
