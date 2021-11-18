42db3c883399c2805ec42448387de7606cdc6af3
SSTinYukk <13999730523@163.com>
2021-11-18 17:15:29 +0800

    test go

---
+++ GO/src/11-reflect/json/json.go
@@ -0,0 +1,5 @@
+package main 
+type Movie struct{
+	Title string 
+	
+}
--- GO/src/11-reflect/reflect3/struct tag.go
+++ GO/src/11-reflect/reflect3/struct tag.go
@@ -1,12 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
 type resume struct{
-	Name string `info:name doc:"我的名字"` 
+	Name string `info:"name" doc:"我的名字"` 
 	Sex  string `info:"sex"`
 }
 
+func findTag(arg interface{}){
+	t:=reflect.TypeOf(arg).Elem()
+
+	for i :=0; i<t.NumField();i++{
+		 tags:=t.Field(i).Tag.Get("info")
+		 fmt.Println("info: ",tags)
+	}
+} 
 
 func main(){
 	var re resume 
+	findTag(&re)
 	
 }
