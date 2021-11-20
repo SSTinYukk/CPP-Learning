5b9444c2e14a8353c0bd3dd5fb828ac8574f0de6
SSTinYukk <13999730523@163.com>
2021-11-20 13:31:09 +0800

    test golang

--- GO/src/14-server-IM-sysytem/server.go
+++ GO/src/14-server-IM-sysytem/server.go
@@ -1,12 +1,44 @@
 package main
-type Server struct{
-	Ip string
-	Prot int 
+
+import (
+	"fmt"
+	"net"
+)
+
+type Server struct {
+	Ip   string
+	Port int
 }
 
-func NewServer(ip string,port int)*Server{
-	server:=&Server{
-		IP: ip,
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:   ip,
+		Port: port,
+	}
+	return server
+}
+
+func (t *Server) Handler(conn net.Conn) {
+	fmt.Println("链接成功...")
+	
+}
+
+//启动服务器的接口
+func (t *Server) Start() {
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", t.Ip, t.Port))
+	if err != nil {
+		fmt.Println("net.Listen error")
+		return
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept error :", err)
+			continue
+		}
+		go t.Handler(conn)
 		
 	}
 }
