cc712479d8256532a4abebde5d2ecbe7856fdb5b
SSTinYukk <13999730523@163.com>
2021-11-21 12:08:03 +0800

    test c++

--- C++/6/6.4.1 重载与作用域.cpp
+++ C++/6/6.4.1 重载与作用域.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
 using namespace std;
+
+typedef struct record{
+    int record;
+    char c;
+}Record;
+
+Record lookup(int &a){
+    cout<<a<<endl;
+}
+
+Record lookup(char &c){
+    cout<<c<<endl;
+}
+
 int main()
 {
 
+    int a=1;
+    char c='c';
+    lookup(a);
+    lookup(c);
 }
---
+++ C++/6/6.5.默认实参.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    typedef string::size_type sz;
+    string screen(sz ht = 24,sz wid =90,char backgran=' ');
+    string windows; 
+
+
+}
