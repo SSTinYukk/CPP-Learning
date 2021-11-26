dbc035a09f79d3e08d57ad3433ea5907fd6a2c0c
SSTinYukk <13999730523@163.com>
2021-11-26 19:36:13 +0800

    Learning C++

--- C++ Prime/7/7.1.2定义改进的Sales_data类.cpp
+++ C++ Prime/7/7.1.2定义改进的Sales_data类.cpp
@@ -12,3 +12,11 @@ struct Sales_data{
 Sales_data add(const Sales_data&,const Sales_data&);
 std::ostream &print(std::ostream&,const Sales_data&);
 std::istream &read(std::istream&,Sales_data&);
+
+double Sales_data::avg_price()const{
+    if (units_sold)
+        return revenue/units_sold;
+    else
+        return 0;
+        
+}
