a8470ca4054456e04357547635d8a438bab33f74
SSTinYukk <13999730523@163.com>
2021-11-26 19:48:50 +0800

    Learning C++

--- C++ Prime/7/7.1.2定义改进的Sales_data类.cpp
+++ C++ Prime/7/7.1.2定义改进的Sales_data类.cpp
@@ -1,5 +1,7 @@
+#ifndef SALES_DATA
+#define SALES_DATA
 #include<iostream>
-
+using namespace std;
 struct Sales_data{
     std::string isbn() const {return this->bookNo;}
     Sales_data&combine(const Sales_data&);
@@ -20,3 +22,36 @@ double Sales_data::avg_price()const{
         return 0;
         
 }
+
+Sales_data& Sales_data::combine(const Sales_data &rhs)
+{
+    units_sold += rhs.units_sold;//把rhs的成员加到this对象的成员上
+    revenue += rhs.revenue;
+    return *this;//返回调用该函数的对象
+}
+
+istream &read(istream &is,Sales_data &item)
+{
+    double price = 0;
+    is>>item.bookNo>>item.units_sold>>price;
+    item.revenue=price*item.units_sold;
+    return is;
+}
+ostream &print(ostream &os,const Sales_data &item)
+{
+    os <<item.isbn()<<" "<<item.units_sold<<" "
+        <<item.revenue<<" "<<item.avg_price();
+        return os;
+    
+}
+
+Sales_data add(const Sales_data &lhs,const Sales_data &rhs)
+{
+    Sales_data sum=lhs;
+    sum.combine(rhs);
+    return sum;
+}
+
+
+
+#endif
