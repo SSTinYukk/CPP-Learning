dc9bf933aa726b206f81254dd176f766b43e5b17
SSTinYukk <13999730523@163.com>
2021-11-26 19:30:00 +0800

    C++ Prime

--- C++ Prime/7/7.1.2定义改进的Sales_data类.cpp
+++ C++ Prime/7/7.1.2定义改进的Sales_data类.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
 
 struct Sales_data{
-    std::string isbn();
-
+    std::string isbn() const {return this->bookNo;}
+    Sales_data&combine(const Sales_data&);
+    double avg_price()const;
 
     std::string bookNo;
     unsigned units_sold=0;
     double revenue = 0.0; 
 };
+Sales_data add(const Sales_data&,const Sales_data&);
+std::ostream &print(std::ostream&,const Sales_data&);
+std::istream &read(std::istream&,Sales_data&);
