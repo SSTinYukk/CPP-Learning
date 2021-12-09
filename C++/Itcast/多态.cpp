4811f8ad7f8673f3f6aa66d04081f216e6322341
SSTinYukk <13999730523@163.com>
2021-12-09 17:34:19 +0800

    C-- Lerning

---
+++ C++/Itcast/多态.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+class Animal{
+public:
+    virtual void Speak(){
+        cout<<"Animal Speaking"<<endl; 
+    }    
+};
+
+class Cat:public Animal{
+public:
+    void Speak(){
+        cout<<"Cat Speaking"<<endl;
+    }
+};
+
+void doSpeak(Animal &animal){
+    animal.Speak();
+}
+
+void test01(){
+    Cat cat;
+    doSpeak(cat);
+   cout<<decltype()<<endl;
+}
+
+int main(){
+    test01();
+}
---
+++ EffectiveC/test.cpp
@@ -0,0 +1,16 @@
+// class GamePlayer{
+// private:
+//     static const int NumTurns=5;
+
+//     enum{ Nums=5};
+//     int socres[NumTurns];    
+// };
+
+// const int GamePlayer::NumTurns;
+
+
+// template<template T>
+// inline void callWith(const T& a,const T& b)
+// {
+//     f(a>b?a:b);
+// }
