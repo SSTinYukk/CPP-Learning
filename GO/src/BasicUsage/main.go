24ffff6dc88846b18fb5e7644c2705c25d7bc978
SSTinYukk <13999730523@163.com>
2022-05-23 10:52:22 +0800

    fdb

--- GO/src/BasicUsage/main.go
+++ GO/src/BasicUsage/main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/apple/foundationdb/bindings/go/src/fdb"
 )
 
 func main() {
-	fmt.Println("program start")
-	fdb.MustAPIVersion(720)
+	// Different API versions may expose different runtime behaviors.
 
-	db := fdb.MUstOPenDefault()
+	// Open the default database from the system cluster
+	db := fdb.MustOpenDefault()
 
-	ret, e := db.Transact(func(tr fdb.Transacttion) (interface{}, error))
+	// Database reads and writes happen inside transactions
+	ret, e := db.Transact(func(tr fdb.Transaction) (interface{}, error) {
+		tr.Set(fdb.Key("hello"), []byte("world"))
+		return tr.Get(fdb.Key("foo")).MustGet(), nil
+		// db.Transact automatically commits (and if necessary,
+		// retries) the transaction
+	})
+	if e != nil {
+		log.Fatalf("Unable to perform FDB transaction (%v)", e)
+	}
+
+	fmt.Printf("hello is now world, foo was: %s\n", string(ret.([]byte)))
 }
