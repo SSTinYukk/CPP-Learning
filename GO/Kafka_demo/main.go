db27b39ff8e9e2b7787ecb2763a32e0e752429a8
SSTinYukk <13999730523@163.com>
2022-07-27 20:36:16 +0800

    kafka demo

---
+++ GO/Kafka_demo/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Shopify/sarama"
+)
+
+func main() {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	config.Producer.Return.Successes = true
+
+	//构造一个消息
+	msg := &sarama.ProducerMessage{}
+	msg.Topic = "web_log"
+	msg.Value = sarama.StringEncoder("this is a test log")
+	//连接Kafka
+	client, err := sarama.NewSyncProducer([]string{"127.0.0.1:9092"}, config)
+	if err != nil {
+		fmt.Println("producer closed,err:", err)
+		return
+	}
+	defer client.Close()
+	pid, offset, err := client.SendMessage(msg)
+	if err != nil {
+		fmt.Println("send msg failed,err:", err)
+		return
+	}
+	fmt.Printf("pid:%v offset:%v\n", pid, offset)
+}
